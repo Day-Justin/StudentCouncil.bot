b0d6bbaaece02cd2d0e759eb75aaa6f5f5bdd5ff
JD <dayjustin.jd@gmail.com>
2022-09-07 15:01:41 -0400

    Bot and client differientaion, hidden file dependencies update

--- StudentCouncil.py
+++ StudentCouncil.py
@@ -6,12 +6,15 @@ import os
 import random
 import logging
 import json
+from dotenv import load_dotenv
 
 
-with open(os.path.dirname(__file__) + '/../config.json') as f:
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+with open(os.path.dirname(__file__) + 'config.json') as f:
     data = json.load(f)
-    TOKEN = data["StudentCouncil"]["TOKEN"]
-    prefix = data["StudentCouncil"]["PREFIX"]
+    PREFIX = data["StudentCouncil"]["PREFIX"]
 
 
 logging.basicConfig(level=logging.INFO)
@@ -24,39 +27,48 @@ logger.addHandler(handler)
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=PREFIX)
 
 
-@bot.event
+@client.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 
 welcome_channel = 1017123907658076252
-@bot.event
+@client.event
 async def on_member_join(member):
     member.guild.channels.get_channel(welcome_channel).send(f'<@{member.id}>Welcome to the server!')
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content == 'hello':
-        await message.channel.send(f'Hi {message.author}')
-    if message.content == 'bye':
-        await message.channel.send(f'Goodbye {message.author}')
-
-    await bot.process_commands(message)
-
-
 # Start each command with the @bot.command decorater
+@bot.command()
+async def info(ctx):
+    """
+    ctx - context
+    (prefix)info
+    """
+    await ctx.send (ctx.guild)
+    await ctx.send (ctx.author)
+    await ctx.send (ctx.message.id)
+
+
 @bot.command()
 async def square(ctx, arg):  # The name of the function is the name of the command
     print(arg)  # this is the text that follows the command
     await ctx.send(int(arg) ** 2)  # ctx.send sends text in chat
 
 
+def get_quote():
+    response = request.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
+
+
+
+
+
+client.run(TOKEN)
 bot.run(TOKEN)
 
